8b188414be45dc1733673edb68d286db5c0f52a7
Joey Jarosz <trucastgaming@gmail.com>
2017-07-30 13:55:27 -0500

    set audio back to false

--- src/public/index.js
+++ src/public/index.js
@@ -8,7 +8,7 @@ const webrtc = new SimpleWebRTC({ // eslint-disable-line
       width: 1280,
       height: 720
     },
-    audio: true
+    audio: false
   },
   autoRequestMedia: true,
   autoRemoveVideos: true

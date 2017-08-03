e1c47de2bd14cafa68d2e5e90d89e530e304fbb2
Joey Jarosz <trucastgaming@gmail.com>
2017-08-03 13:17:01 -0500

    fix signal url

--- src/public/index.js
+++ src/public/index.js
@@ -1,7 +1,7 @@
 let ready_for_call = false;
 const room = location.search && location.search.split('?')[1];
 const webrtc = new SimpleWebRTC({ // eslint-disable-line
-  url: 'https://localhost:8888',
+  // url: 'https://localhost:8888',
   localVideoEl: 'videos',
   remoteVideosEl: '',
   media: {
